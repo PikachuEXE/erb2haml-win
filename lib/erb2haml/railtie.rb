44e6e14177edb2b40deda842df1f123d9ee9078d
PikachuEXE <pikachuexe@gmail.com>
2011-12-29 21:40:33 +0800

    Trying to fix...

--- lib/erb2haml/railtie.rb
+++ lib/erb2haml/railtie.rb
@@ -1,10 +1,10 @@
-require 'erb2haml'
+require 'erb2haml-win'
 require 'rails'
 
 module ERb2Haml
   class Railtie < Rails::Railtie
     rake_tasks do
-      load 'erb2haml/railties/erb2haml.rake'
+      load 'erb2haml-win/railties/erb2haml.rake'
     end
   end
 end
